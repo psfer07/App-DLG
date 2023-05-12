b6487dd1f0e25f894ceec9ab29d4c591daeabf7e
psfer07 <102543836+psfer07@users.noreply.github.com>
2023-05-12 20:09:45 +0200

    Minor improvements

--- xml-script.ps1
+++ xml-script.ps1
@@ -1,3 +1,14 @@
+# Declare functions
+function Get-AppSize {
+  param[([int]$length)]
+  $request = Invoke-WebRequest $url -Method Head
+  $length = [int]$request.Headers['Content-Length']
+if ($length -gt 1GB) { [string]::Format("{0:0.00} GB", $length / 1GB) }
+elseIf ($length -gt 1MB) { [string]::Format("{0:0.00} MB", $length / 1MB) }
+elseIf ($length -gt 1KB) { [string]::Format("{0:0.00} kB", $length / 1KB) }
+elseIf ($length -gt 0) { [string]::Format("{0:0.00} B", $length) }
+}
+
 # Bypasses any execution policy
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
 
@@ -29,7 +40,7 @@ $nameArray = $json.psobject.Properties.Name
 $filteredApps = @()
 foreach ($i in 0..($nameArray.Count - 1)) {
   $name = $nameArray[$i]; $app = $json.$name; $folder = $app.folder; $url = $app.URL; $exe = $app.exe; $syn = $app.syn; $cmd = $app.cmd; $cmd_syn = $app.cmd_syn
-  $filteredApps += [PsCustomObject]@{Index = $i; Name = $name; Folder = $folder; URL = $url; Exe = $exe; Size = $size; Syn = $syn; Cmd = $cmd; Cmd_syn = $cmd_syn }
+  $filteredApps += [PsCustomObject]@{Index = $i; Name = $name; Folder = $folder; URL = $url; Exe = $exe; Size = $length; Syn = $syn; Cmd = $cmd; Cmd_syn = $cmd_syn }
 }
 
 # Lists every single app in the JSON
@@ -39,7 +50,7 @@ foreach ($i in 0..($filteredApps.Count - 1)) {
 }
 
 # Assign the corresponding variables to the selected app
-$n = $filteredApps[$($WPFprogram.SelectedValue)]; $program = $n.Name; $exe = $n.Exe; $syn = $n.Syn; $folder = $n.folder; $url = $n.URL; $cmd = $n.Cmd; $cmd_syn = $n.Cmd_syn; #$o = Split-Path $url -Leaf
+$n = $filteredApps[$($WPFprogram.SelectedValue)]; $program = $n.Name; $exe = $n.Exe; $syn = $n.Syn; $folder = $n.folder; $url = $n.URL; $cmd = $n.Cmd; $cmd_syn = $n.Cmd_syn;$size = Get-AppSize $length; #$o = Split-Path $url -Leaf
 
 Write-Host "$program selected"
 Start-Sleep -Milliseconds 2500
@@ -58,22 +69,7 @@ Write-Host "You are going to download$openString $program"
 #$dl = Read-Host 'Confirmation (press any key or go to the (R)estart menu)'
 #Invoke-RestMethod -Uri $url -OutFile "$p\$o"
 
-
-
-#Assign the corresponding variables to the selected app
-$app = $filteredApps[$($WPFprogram.SelectedValue)]
-$program = $app.Name
-$exe = $app.Exe
-$folder = $app.folder
-$url = $app.URL
-
-if ($size -gt 1GB) { [string]::Format("{0:0.00} GB", $size / 1GB) }
-elseIf ($size -gt 1MB) { [string]::Format("{0:0.00} MB", $size / 1MB) }
-elseIf ($size -gt 1KB) { [string]::Format("{0:0.00} kB", $size / 1KB) }
-elseIf ($size -gt 0) { [string]::Format("{0:0.00} B", $size) }
-
-$WPFsyn.Text = "$program's properties:
-$program is $syn
+$WPFsyn.Text = "It is $syn
 Size: $size
 Saved in : $folder
 Recommended parameters do: $cmd_syn
@@ -83,6 +79,7 @@ Parameters are: $cmd"
 $WPFprogram.Add_SelectionChanged({})
 
 if ($WPFopen.IsChecked){$open = $true}
+if ($open -eq $true){ Open-File } else {exit}
 
 $WPFdownload.Add_Click({
     Write-Host $exe
